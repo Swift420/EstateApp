93cf472332110f5d6860411cfa4dab982f14b9bf
Apollos David <apollos.david@adaire.com>
2024-06-10 10:57:17 +0200

    single page

---
+++ client/src/lib/loader.js
@@ -0,0 +1,14 @@
+import apiRequest from "./apiRequest"
+
+export const singlePageLoader = async({request,params}) => {
+    const res = await apiRequest.get("/posts/"+params.id)
+
+    return res.data
+}
+
+export const listPageLoader = async({request,params}) => {
+        console.log(request)
+    // const res = await apiRequest.get("/posts/"+params.id)
+
+    // return res.data
+}
